67addf5dc2381a00c185fd6128147c4ffdcf2d63
Samuel Sanchez <wearycodes@gmail.com>
2023-11-08 23:25:25 -0600

    Todo logic moved to its own filke

--- frontend/components/TodoList.js
+++ frontend/components/TodoList.js
@@ -1,11 +1,15 @@
 import React from 'react'
-
+import Todo from './Todo'
 export default class TodoList extends React.Component {
   render() {
     return (
       <div id = "todos">
       {this.props.todos.reduce((acc, td) => {
-        if (this.props.displayCompleteds || !td.completed) return acc.concat(<div onClick={this.props.toggleCompleted(td.id)} key={td.id}>{td.name}{td.completed ? "☑️" : ""}</div>)
+        if (this.props.displayCompleteds || !td.completed) return acc.concat(<Todo
+          toggleCompleted = {this.props.toggleCompleted}
+          todo = {td}
+          key = {td.id}
+          />)
         return acc
       }, [])
         // return <div onClick={this.toggleCompleted(todo.id)} key={todo.id}>{todo.name}{todo.completed ? "☑️" : ""}</div>
